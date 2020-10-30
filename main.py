e3ef4338121dc7d29204044db6b5bd0db1a113eb
Dove6 <dawidsygocki1234@wp.pl>
2020-10-31 00:13:42 +0100

    Make slides update according to audio position
    Add seeking 5 secs forward/backward on right/left arrow key press.

--- main.py
+++ main.py
@@ -7,7 +7,7 @@ def parse_line(line):
     if line.count('@') != 1:
             raise Exception()
     time = line[:line.find('@')].strip()
-    page = line[(line.find('@')+1):].strip()
+    page = int(line[(line.find('@')+1):].strip())
 
     time_parts = {'hours': 0, 'mins': 0, 'secs': 0, 'msecs': 0}
     if (match := re.fullmatch(r'(\d+):(\d{2}):(\d{2})(\.(\d{1,3}))?', time)) is not None:
@@ -38,6 +38,11 @@ def parse_description(filename):
             timestamps.append(parse_line(line))
     return sorted(timestamps, key=(lambda x: x['timestamp']))
 
+def time_to_index(description, ms):
+    description = list(filter(lambda x: x['timestamp'] < ms, description))
+    if len(description) > 0:
+        return (description[-1]['pagename'] - 1)
+
 
 class Application:
     def __init__(self, width, height, paths):
@@ -47,13 +52,16 @@ class Application:
         self.paths = dict(filter(lambda x: x[0] in ['audio', 'description', 'pdf'], paths.items()))
         self.description = parse_description(paths['description'])
         self.pdf = fitz.open(paths['pdf'])
+        self.page_num = None
         self.player = mpv.MPV()
         self.player.play(paths['audio'])
         self.player.video = 'no'
         self.player.pause = True
         self.player.keep_open = True
-        # @self.player.property_observer('time-pos')
-        # def fun(_name, value)
+        @self.player.property_observer('time-pos')
+        def slide_changer(_name, value):
+            if value is not None:
+                self.update_page(time_to_index(self.description, value * 1000))
         self.tkroot = tk.Tk()
         self.tkroot.title(paths['pdf'])
         self.tkroot.bind('<Key>', self.key_callback)
@@ -61,13 +69,18 @@ class Application:
         self.tkcanvas.pack()
         self.tkroot.withdraw()
         self.tkimage = None
-        self.page_num = None
     
     def key_callback(self, event):
         if event.keysym in ['Left', 'Right']:
+            if self.player.seekable:
+                if event.keysym == 'Left':
+                    self.player.seek(-5)
+                else:
+                    self.player.seek(5)
+        if event.keysym in ['Up', 'Down']:
             index = self.page_num
             if index is not None:
-                if event.keysym == 'Left':
+                if event.keysym == 'Down':
                     self.update_page(index - 1)
                 else:
                     self.update_page(index + 1)
@@ -78,19 +91,25 @@ class Application:
 
     def update_page(self, index):
         # print(index)
-        if 0 <= index < len(self.pdf):
-            self.tkcanvas.delete('slide')
-            page = self.pdf[index]
-            rect = page.rect
-            matrix = fitz.Matrix()
-            matrix.a = matrix.d = min(self.width / (rect[2] - rect[0]), self.height / (rect[3] - rect[1]))
-            matrix.e = -rect[0]
-            matrix.f = -rect[1]
-            pix = page.getPixmap(matrix)
-            self.tkimage = tk.PhotoImage(data=pix.getImageData('ppm'))
-            self.tkcanvas.create_image((self.width - pix.width) / 2, (self.height - pix.height) / 2,
-                anchor=tk.NW, image=self.tkimage, tags='slide')
-            self.page_num = index
+        if index is None:
+            if self.page_num is not None:
+                self.tkcanvas.delete('slide')
+                self.tkimage = None
+                self.page_num = None
+        elif 0 <= index < len(self.pdf):
+            if index != self.page_num:
+                self.tkcanvas.delete('slide')
+                page = self.pdf[index]
+                rect = page.rect
+                matrix = fitz.Matrix()
+                matrix.a = matrix.d = min(self.width / (rect[2] - rect[0]), self.height / (rect[3] - rect[1]))
+                matrix.e = -rect[0]
+                matrix.f = -rect[1]
+                pix = page.getPixmap(matrix)
+                self.tkimage = tk.PhotoImage(data=pix.getImageData('ppm'))
+                self.tkcanvas.create_image((self.width - pix.width) / 2, (self.height - pix.height) / 2,
+                    anchor=tk.NW, image=self.tkimage, tags='slide')
+                self.page_num = index
         else:
             print('Out of range')
 
